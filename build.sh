5c100029cfc9e1cf10370d49e0b368d2b22daa9f
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-07-02 20:44:39 -0400

    Add files via upload

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 clear
 sudo cp -r -p Source/. Build
 cd Build
-g++ -o Main Main.c MapGenerator.c DrawScreen.c Player.c Movement.c DrawUI.c PlayerCreator.c Planets.c Enemies.c Inventory.c Items.c FileRead.c Gas.c Legacy.c Utils.c History.c -lm -lncurses -L../Source/FastNoiseLite.h
-./Main
+# you can replace "Linux-Verirogue" with whatever you want, I just have it this way cause I develop the game on linux
+g++ -o Linux-Verirogue Main.c MapGenerator.c DrawScreen.c Player.c Movement.c DrawUI.c PlayerCreator.c Planets.c Enemies.c Inventory.c Items.c FileRead.c Gas.c Legacy.c Utils.c History.c -lm -lncurses -L../Source/FastNoiseLite.h
+./Linux-Verirogue
 find . -type f -name '*.c' -delete
 find . -type f -name '*.h' -delete
